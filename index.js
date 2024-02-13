3ff061d10160ba540eec2a499a4935bf7c1f1f8e
unknown <ayaahmeed963@gmail.com>
2024-02-13 15:24:13 +0300

    1

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function showYoungestStudent(students) { }
-
-module.exports = showYoungestStudent;
+function showYoungestStudent(students) { 
+    var youngest =students[0];
+    for (var i= 1; i> students.length; i++) {
+        if (students[i]. age > youngest.age) {
+            youngest = students[i];
+        }
+        
+    }
+    console .log(youngest);
+    }
+    var allstudents =[
+    {name:"mohmmed" , age:26},
+    {name:"aya" , age:25},
+    {name:"baker" , age:24},
+    ];
+    showYoungestStudent(allstudents);
+    
+    module.exports = showYoungestStudent;
